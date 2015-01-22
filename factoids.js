2edb77d94365d78e821f87e52c243eee726220ef
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-01-22 03:25:51 +0100

    Rewrite, remember who set factoids, lock factoids

--- factoids.js
+++ factoids.js
@@ -1,20 +1,160 @@
+/**
+ * Dirty DB is an append only database that emulates a JSON object.
+ *
+ * Our database has keys of factoid names, with a value being the
+ * following struct:
+ *
+ * %Factoid{
+ *   intent: %Tennu.Message{}'s intent ^1
+ *   message: %Tennu.Message{}'s message ^1
+ *   editor: Full hostmask of last editor to the factoid.
+ *   time: Time of modification.
+ *   frozen: Boolean of whether only admins can edit the factoid.
+ * }
+ *
+ * 1: Will be missing if the message is deleted.
+ **/
+
+ // Dirty DB really needs an update method...
+
 const Dirty = require('dirty');
+const Promise = require('bluebird');
 const format = require('util').format;
+const now = function () { return (new Date()).toISOString(); }
 
 module.exports = function (databaseLocation) {
     const db = Dirty(databaseLocation);
 
     return {
+        // String -> %Tennu.Message{}
         get: function (key) {
-            return db.get(key.toLowerCase());
+            const value = db.get(key.toLowerCase());
+
+            if (!value || !value.message) {
+                return;
+            }
+
+            return {
+                intent: value.intent,
+                message: value.message
+            };
         },
 
-        set: function (key, value) {
-            db.set(key.toLowerCase(), value);
+        // String, %Factoid{} -> Boolean
+        // Boolean represents whether key was actually set.
+        // A key can be not set if the key is frozen and
+        //   the editor is not an admin.
+        set: function (key, next) {
+            return new Promise(function (resolve, reject) {
+                if (!(next.intent && next.message && next.editor)) {
+                    reject(new Error("An addition or modification to the database requires an intent, message, and editor."));
+                    return;
+                }
+
+                function _set () {
+                    db.set(key.toLowerCase(), {
+                        intent: next.intent,
+                        message: next.message,
+                        editor: next.editor,
+                        time: now(),
+                        frozen: previous ? previous.frozen : false
+                    });
+                }
+
+                const previous = db.get(key);
+
+                console.log(require('util').inspect(previous));
+                console.log(require('util').inspect(next));
+
+                if (previous && previous.frozen) {
+                    return next.isAdmin(next.editor).then(function (isAdmin) {
+                        if (isAdmin) {
+                            _set();
+                            resolve(true);
+                        } else {
+                            resolve(false);
+                        }
+                    });
+                }
+
+                _set();
+                resolve(true);
+            });
         },
 
-        delete: function (key) {
-            db.rm(key.toLowerCase());
+        // String, Hostmask -> Boolean
+        // Boolean represents whether key was actaully deleted.
+        // A key can only be deleted if it has a message, and
+        //   if it is frozen, that the editor is an admin.
+        delete: function (key, editor, isAdmin) {
+            return new Promise(function (resolve, reject) {
+                key = key.toLowerCase();
+                const previous = db.get(key);
+
+                function _delete () {
+                    db.set(key, {
+                        editor: editor,
+                        time: now(),
+                        frozen: previous.frozen
+                    });
+                }
+
+                if (!previous || !previous.message) {
+                    reject([false, "dne"]);
+                } else if (previous && previous.frozen) {
+                    isAdmin(editor).then(function (isAdmin) {
+                        if (isAdmin) {
+                            _delete();
+                            resolve([true]);
+                        } else {
+                            resolve([false, "frozen"]);
+                        }
+                    }).catch(reject);
+                } else {
+                    _delete();
+                    resolve([true]);
+                }
+            });
+        },
+
+        // String -> Boolean
+        freeze: function (key) {
+            const value = db.get(key);
+
+            if (!value) {
+                db.set(key, {
+                    frozen: true
+                });
+            }
+
+            db.set(key, {
+                intent: value.intent,
+                message: value.message,
+                editor: value.editor,
+                time: value.time,
+                frozen: true
+            });
+
+            return true;
+        },
+
+        // String -> Boolean
+        unfreeze: function (key) {
+            const value = db.get(key);
+
+            if (!value) {
+                return false;
+            }
+
+            db.set(key, {
+                intent: value.intent,
+                message: value.message,
+                editor: value.editor,
+                time: value.time,
+                frozen: false
+            });
+
+            return true;
         }
     };
 };
--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
 const Factoids = require("./factoids");
 const format = require('util').format;
+const Promise = require('bluebird');
 
 const splitAt = function (string, match) {
     const matchIx = string.indexOf(match);
@@ -33,6 +34,18 @@ module.exports = {
 
         const factoids = Factoids(database, client.debug.bind(client, "factoids"));
 
+        const adminPlugin = client.getRole("admin");
+
+        console.log(require('util').inspect(adminPlugin));
+
+        var requiresAdmin, isAdmin;
+        if (adminPlugin) {
+            requiresAdmin = adminPlugin.requiresAdmin;
+            isAdmin = adminPlugin.isAdmin;
+        } else {
+            isAdmin = function () { return Promise.resolve(false); }
+        }
+
         // Privmsg -> Bool
         function isFactoidRequest(privmsg) {
             return privmsg.message.indexOf(factoidTrigger) === 0;
@@ -59,106 +72,168 @@ module.exports = {
             }
         }
 
-        return {
-            handlers: {
-                privmsg: function (privmsg) {
-                    if (isFactoidRequest(privmsg)) {
-                        client.note("factoids", "getting factoid: " + privmsg.message);
-                        return getFactoid(getFactoidKey(privmsg.message), false);
-                    }
-                },
-
-                "!factoid": function (command) {
-                    if (command.args.length === 0) {
-                        return "No factoid specified.";
-                    }
-
-                    client.note("factoids", "getting factoid: " + command.args.join(" "));
-                    return getFactoid(command.args.join(" "), command.channel, true);
-                },
-
-                "!learn": function (command) {
-                    // args is [factoid, description]
-                    const args = splitAt(command.args.join(" "), "=");
-                    const key = trim(args[0])
-                    const value = function (input) {
-                        const trimmed = trim(args[1]);
-                        const lowered = trimmed.toLowerCase();
-
-                        if (startsWith(lowered, actPrefix)) {
-                            return {
-                                intent: "act",
-                                message: trimmed.slice(actPrefix.length)
-                            };
-                        } else if (startsWith(lowered, replyPrefix)) {
-                            return {
-                                intent: "say",
-                                message: trimmed.slice(replyPrefix.length)
-                            };
-                        } else if (startsWith(lowered, aliasPrefix)) {
-                            return {
-                                intent: "alias",
-                                message: trimmed.slice(aliasPrefix.length)
-                            }
-                        } else {
-                            return {
-                                intent: "say",
-                                message: format("%s is %s", key, trimmed)
-                            };
-                        }
-                    }(args[1]);
-
-                    if (key === "") {
-                        return "Invalid format. Missing key.";
-                    }
-
-                    if (value === "") {
-                        return "Invalid format. Missing '=' or factoid description.";
-                    }
-
-                    client.note("factoids", format("learning that %s = %s", key, value));
-                    factoids.set(key, value);
-
-                    return format("Learned factoid '%s'", key);
-                },
-
-                "!forget": function (command) {
-                    if (command.args.length === 0) {
-                        return "No factoid specified.";
-                    }
-
-                    const key = command.args.join(" ");
-                    client.note("factoids", format("forgetting: %s", key));
-
-                    if (!factoids.get(key)) {
-                        return format("Factoid by the name '%s' does not exist.", key)
-                    }
-                    factoids.delete(command.args.join(" "));
-
-                    return format("Forgotten factoid by name of '%s'", key);
+        const handlers = {
+            privmsg: function (privmsg) {
+                if (isFactoidRequest(privmsg)) {
+                    client.note("factoids", "getting factoid: " + privmsg.message);
+                    return getFactoid(getFactoidKey(privmsg.message), false);
                 }
             },
 
-            help: {
-                "factoid": [
-                    "!factoid factoid-name",
-                ],
+            "!factoid": function (command) {
+                if (command.args.length === 0) {
+                    return "No factoid specified.";
+                }
 
-                "learn": [
-                    "!learn factoid-name = faction-description",
-                    "",
-                    "Adds a factoid to the factoids database.",
-                    "Can also change a factoid.",
-                    "",
-                    "Use <reply> "
-                ],
-
-                "forget": [
-                    "!forget factoid-name"
-                ]
+                client.note("factoids", "getting factoid: " + command.args.join(" "));
+                return getFactoid(command.args.join(" "), command.channel, true);
             },
 
-            commands: ["factoid", "learn, forget"]
+            "!learn": function (command) {
+                // args is [key, description]
+                const args = splitAt(command.args.join(" "), "=");
+                const key = trim(args[0]);
+                const description = function (input) {
+                    const trimmed = trim(input);
+                    const lowered = trimmed.toLowerCase();
+
+                    if (trimmed.length === 0) {
+                        return {message: ""};
+                    }
+
+                    if (startsWith(lowered, actPrefix)) {
+                        return {
+                            intent: "act",
+                            message: trimmed.slice(actPrefix.length),
+                            editor: command.hostmask,
+                            isAdmin: isAdmin
+                        };
+                    } else if (startsWith(lowered, replyPrefix)) {
+                        return {
+                            intent: "say",
+                            message: trimmed.slice(replyPrefix.length),
+                            editor: command.hostmask,
+                            isAdmin: isAdmin
+                        };
+                    } else if (startsWith(lowered, aliasPrefix)) {
+                        return {
+                            intent: "alias",
+                            message: trimmed.slice(aliasPrefix.length),
+                            editor: command.hostmask,
+                            isAdmin: isAdmin
+                        };
+                    } else {
+                        return {
+                            intent: "say",
+                            message: format("%s is %s", key, trimmed),
+                            editor: command.hostmask,
+                            isAdmin: isAdmin
+                        };
+                    }
+                }(args[1]);
+
+                if (key === "") {
+                    return "Invalid format. No key specified.";
+                }
+
+                if (description.message === "") {
+                    return "Invalid format. Missing either '=' or factoid description.";
+                }
+
+                return factoids.set(key, description).then(function (success) {
+                    if (success) {
+                        client.note("FactoidsPlugin", format("Factoid: '%s' => [%s] %s", key, description.intent, description.message));
+                        return format("Learned factoid '%s'", key);
+                    } else {
+                        return format("Cannot (re)learn factoid '%s'! Factoid is locked.", key);
+                    }
+                }).catch(function (err) {
+                    client.say(command.channel, "Error: Internal Error.");
+                    throw err;
+                });
+            },
+
+            "!forget": function (command) {
+                if (command.args.length === 0) {
+                    return "No factoid specified.";
+                }
+
+                const key = command.args.join(" ");
+                client.note("factoids", format("forgetting: %s", key));
+
+                if (!factoids.get(key)) {
+                    return format("Factoid '%s' does not exist.", key);
+                }
+
+                return factoids.delete(key, command.hostmask, isAdmin).then(function (res) {
+                    const success = res[0];
+                    const failReason = res[1];
+                    if (success) {
+                        return format("Forgotten factoid '%s'", key);
+                    } else if (failReason === "frozen") {
+                        return format("Cannot forget factoid '%s'. Factoid is locked.", key);
+                    } else {
+                        return format("Factoid by the key '%s' does not exist.", key);
+                    }
+                });
+            }
+        };
+
+        const helpfiles = {
+            "factoid": [
+                "{{!}}factoid factoid-name",
+                "",
+                "Look up a factoid."
+            ],
+
+            "learn": [
+                "{{!}}learn factoid-name = faction-description",
+                "",
+                "Adds a factoid to the factoids database.",
+                "Can also change a factoid.",
+                "",
+                "Use <reply> to leave out the '{{factoid-name}} is' prefix.",
+                "Use <act> to make the bot respond as an action."
+            ],
+
+            "forget": [
+                "{{!}}forget factoid-name"
+            ]
+        };
+
+        if (requiresAdmin) {
+            handlers["!lock"] = requiresAdmin(function (command) {
+                const factoid = command.args.join(" ").toLowerCase()
+                factoids.freeze(factoid);
+                return format("Locked factoid '%s'.", factoid);
+            });
+
+            helpfiles["lock"] = [
+                "{{!}}lock factoid-name",
+                "",
+                "Locks a factoid so only an admin can edit it.",
+                "Requires admin privileges.",
+                "Use {{!}}unlock to undo this."
+            ];
+
+            handlers["!unlock"] = requiresAdmin(function (command) {
+                const factoid = command.args.join(" ").toLowerCase()
+                factoids.unfreeze(factoid);
+                return format("Locked factoid '%s'.", factoid);
+            });
+
+            helpfiles["unlock"] = [
+                "{{!}}unlock factoid-name",
+                "",
+                "Unlocks a locked factoid so that anybody can edit it.",
+                "Requires admin privileges."
+            ];
+        }
+
+        return {
+            handlers: handlers,
+            help: helpfiles,
+            commands: Object.keys(handlers).map(function (command) { return command.slice(1); })
         };
     }
 };
